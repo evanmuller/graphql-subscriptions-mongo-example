420518f608481f2eb0bf400f88407036c2c55e67
Evan Muller <evanmuller@gmail.com>
2020-02-19 10:38:40 -0500

    Changed the subscription api to include all mutations filtered by day

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,5 @@
 const moment = require("moment");
+const { withFilter } = require("graphql-yoga");
 const { Task } = require("./models");
 
 const updateTask = async (id, updates, pubsub) => {
@@ -15,7 +16,12 @@ const updateTask = async (id, updates, pubsub) => {
     },
   );
 
-  pubsub.publish("updatedTask", { updatedTask: task });
+  pubsub.publish("taskOnDay", {
+    taskOnDay: {
+      mutation: "UPDATED",
+      task,
+    },
+  });
 
   return task;
 };
@@ -55,7 +61,14 @@ const resolvers = {
       });
 
       await task.save();
-      pubsub.publish("newTask", { newTask: task });
+
+      pubsub.publish("taskOnDay", {
+        taskOnDay: {
+          mutation: "CREATED",
+          task,
+        },
+      });
+
       return task;
     },
     updateTask: (_, { id, name, date, complete }, { pubsub }) =>
@@ -64,26 +77,37 @@ const resolvers = {
     completeTask: (_, { id, complete }, { pubsub }) =>
       updateTask(id, { complete }, pubsub),
     deleteTask: async (_, { id }, { pubsub }) => {
-      await Task.findOneAndDelete({ _id: id });
-      pubsub.publish("deletedTask", { deletedTask: id });
+      const task = await Task.findOneAndDelete({ _id: id });
+
+      pubsub.publish("taskOnDay", {
+        taskOnDay: {
+          mutation: "DELETED",
+          task,
+        },
+      });
+
       return true;
     },
   },
   Subscription: {
-    newTask: {
-      subscribe: (_, {}, { pubsub }) => {
-        return pubsub.asyncIterator("newTask");
-      },
-    },
-    updatedTask: {
-      subscribe: (_, {}, { pubsub }) => {
-        return pubsub.asyncIterator("updatedTask");
-      },
-    },
-    deletedTask: {
-      subscribe: (_, {}, { pubsub }) => {
-        return pubsub.asyncIterator("deletedTask");
-      },
+    taskOnDay: {
+      subscribe: withFilter(
+        (_, {}, { pubsub }) => pubsub.asyncIterator("taskOnDay"),
+        (taskSubscriptionPayload, variables) => {
+          const taskDate = moment.utc(
+            taskSubscriptionPayload.taskOnDay.task.date,
+          );
+
+          const { day } = variables;
+
+          return taskDate.isBetween(
+            moment.utc(day).startOf("day"),
+            moment.utc(day).endOf("day"),
+            null,
+            "[]",
+          );
+        },
+      ),
     },
   },
 };
